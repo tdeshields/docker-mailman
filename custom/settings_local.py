25f89ec265ac6fb1d61c63135966e43375a145ff
tdeshields <61873965+tdeshields@users.noreply.github.com>
2023-07-11 10:44:49 -0500

    Update settings_local.py
    Local settings.py for mailman3-django that is pulled when container starts

--- custom/settings_local.py
+++ custom/settings_local.py
@@ -1,1 +1,26 @@
+#Disabling user sign-up. Already existing users are still allowed to login.
+ACCOUNT_ADAPTER = 'django_mailman3.views.user_adapter.DisableSignupAdapter'
+# Enable debug if needed
+# DEBUG = True
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ALLOWED_HOSTS = ['*']
 
+# cross site cookies
+CSRF_TRUSTED_ORIGINS = [
+        "https://listdev.usm.edu",
+]
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'xapian_backend.XapianEngine',
+        'PATH': "/opt/mailman-web-data/fulltext_index",
+    },
+}
+#Disable social SSO login
+MAILMAN_WEB_SOCIAL_AUTH = []
+
+# disabling email verification
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+MAILMAN_EMAIL_RESTRICTIONS = 'none'
+
+# setting time zone globally
+TIME_ZONE = 'America/Chicago'
