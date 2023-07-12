c8ae9c6985012075f496699365bb81f1ae836cbc
tdeshields <61873965+tdeshields@users.noreply.github.com>
2023-07-12 08:58:29 -0500

    Update env_setup.sh

--- custom/env_setup.sh
+++ custom/env_setup.sh
@@ -2,7 +2,7 @@
 
 # this script will setup the environment on a fresh machine for the docker-mailman git project. 
 
-# checking for and setting up the mailman-web local directories
+# checking for or setting up the mailman-web local directories
 if [ -d "/opt/mailman/web" ]
 	exit 0
 else
@@ -10,16 +10,22 @@ else
 fi
 
 
-# checking for and setting up mailman-core local directories
+# checking for or setting up mailman-core local directories
 if [ -d "/opt/mailman/core" ]
 	exit 0
 else
 	mkdir -p /opt/mailman/core
 fi
 
+# Checking for or setting up the directory for our database backup
+if [ -d "/opt/backup" ]
+	exit 0
+else
+	mkdir -p /opt/backup
+fi
 
-# changing to working dir and moving all the config files in place 
+# changing to working dir and moving all the config files in the proper place
 cd /opt/mailman/docker-mailman
-
 cp custom/settings_local.py ../web/settings_local.py
 cp custom/mailman-extra.cfg ../core/mailman-extra.cfg
+
