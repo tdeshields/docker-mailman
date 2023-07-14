aa2ddeb4049edce1a85bf8f16c7b8975e1dde9be
tdeshields <61873965+tdeshields@users.noreply.github.com>
2023-07-14 15:08:40 -0500

    Update email_verify_trigger.py

--- custom/email_verify_trigger.py
+++ custom/email_verify_trigger.py
@@ -1,1 +1,17 @@
+from django.contrib.auth.signals import user_logged_in
+from django.dispatch import receiver
+from django.contrib.auth.models import User
 
+'''
+This script will trigger when a ldap user logs in for the first time
+and make an API call to verify the user's email address
+'''
+
+
+
+@receiver(user_logged_in)
+def email_verify(sender, request, user, **kwargs):
+    if user.last_login is None:
+        # code to verify email
+        email = user.email
+        
