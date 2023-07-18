937ef4e0585693c67b3905ccf92a0536e3173230
tdeshields <61873965+tdeshields@users.noreply.github.com>
2023-07-18 10:46:42 -0500

    Update email_verify.sh

--- custom/email_verify.sh
+++ custom/email_verify.sh
@@ -8,10 +8,10 @@
 while getopts ":vu:" option; do
   case $option in
     v)
-      podman exec -it mailman-core curl --user restadmin:restpass -X POST http://10.89.0.53:8001/3.1/addresses/$1/verify #| python3 -m json.tool
+      podman exec -it mailman-core curl --user restadmin:restpass -X POST http://mailman-core:8001/3.1/addresses/$1/verify #| python3 -m json.tool
       exit;;
     u)
-      podman exec -it mailman-core curl --user restadmin:restpass -X POST http://10.89.0.53:8001/3.1/addresses/$1/unverify #| python3 -m json.tool
+      podman exec -it mailman-core curl --user restadmin:restpass -X POST http://mailman-core:8001/3.1/addresses/$1/unverify #| python3 -m json.tool
       exit;;
     *)
       echo "invalid option"
