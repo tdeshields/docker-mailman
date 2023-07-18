c3bbeef3c0273358a71ccae4f388e4d481fec188
tdeshields <61873965+tdeshields@users.noreply.github.com>
2023-07-18 11:24:14 -0500

    Update email_verify.sh

--- custom/email_verify.sh
+++ custom/email_verify.sh
@@ -1,22 +1,47 @@
 #!/bin/bash
 
-# curl --user restadmin:restpass -X POST http://10.89.0.53:8001/3.1/addresses/$1/verify #| python3 -m json.tool
-
-# curl --user restadmin:restpass -X POST http://10.89.0.53:8001/3.1/addresses/$1/unverify #| python3 -m json.tool
+# this script will send a POST to the rest API to set email verification based on your option. You can also use the list option to query the API for user info to validate.
 
 
-while getopts ":vul:" option; do
+function print_help {
+        echo "Usage: $0 [-v|-u|-l] email_address"
+        echo "Options:"
+        echo "  -v      Verify an email address within the API"
+        echo "  -u      Unverify an email address within the API"
+        echo "  -l      Query and list the user's API info"
+        echo "  -h      Print the help menu"
+        exit 1
+}
+
+
+if [ $# -eq 0 ];
+then
+        print_help
+fi
+
+
+
+while getopts ":vul:h" option; do
   case $option in
     v)
-      podman exec -it mailman-core curl --user restadmin:restpass -X POST http://mailman-core:8001/3.1/addresses/$1/verify #| python3 -m json.tool
+      podman exec -it mailman-core curl --user restadmin:restpass -X POST http://mailman-core:8001/3.1/addresses/$2/verify #| python3 -m json.tool
       exit;;
     u)
-      podman exec -it mailman-core curl --user restadmin:restpass -X POST http://mailman-core:8001/3.1/addresses/$1/unverify #| python3 -m json.tool
+      podman exec -it mailman-core curl --user restadmin:restpass -X POST http://mailman-core:8001/3.1/addresses/$2/unverify #| python3 -m json.tool
       exit;;
     l)
       podman exec -it mailman-core curl --user restadmin:restpass -X GET http://mailman-core:8001/3.1/addresses/$2 | python3 -m json.tool
-    *)
-      echo "invalid option"
       exit;;
+    h)
+      print_help
+      ;;
+    *)
+      echo "invalid option: -$OPTARG"
+      print_help
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument."
+      print_help
+      ;;
   esac
 done
