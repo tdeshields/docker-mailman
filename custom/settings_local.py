1d110e0809b95fef9d8ddafea4dcd13751c99084
tdeshields <61873965+tdeshields@users.noreply.github.com>
2023-09-13 08:43:04 -0500

    Update settings_local.py

--- custom/settings_local.py
+++ custom/settings_local.py
@@ -1,13 +1,14 @@
 #Disabling user sign-up. Already existing users are still allowed to login.
-ACCOUNT_ADAPTER = 'django_mailman3.views.user_adapter.DisableSignupAdapter'
-# Enable debug if needed
+# ACCOUNT_ADAPTER = 'django_mailman3.views.user_adapter.DisableSignupAdapter'
+
+
 # DEBUG = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 ALLOWED_HOSTS = ['*']
 
 # cross site cookies
 CSRF_TRUSTED_ORIGINS = [
-        "https://listdev.usm.edu",
+        "https://lists.usm.edu",
 ]
 HAYSTACK_CONNECTIONS = {
     'default': {
@@ -15,44 +16,56 @@ HAYSTACK_CONNECTIONS = {
         'PATH': "/opt/mailman-web-data/fulltext_index",
     },
 }
+
 #Disable social SSO login
-MAILMAN_WEB_SOCIAL_AUTH = []
+# MAILMAN_WEB_SOCIAL_AUTH = []
 
 # disabling email verification
-ACCOUNT_EMAIL_VERIFICATION = 'none'
-MAILMAN_EMAIL_RESTRICTIONS = 'none'
+# ACCOUNT_EMAIL_VERIFICATION = None
 
-# setting time zone globally
-TIME_ZONE = 'America/Chicago'
+# setting time zone in Django
+TIME_ZONE = 'UTC'
+USE_TZ = True
 
 # settings login session cookies variables
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 SESSION_COOKIE_AGE = 600
 
+
+# custom python script to automatically verify email
+# ACCOUNT_ADAPTER = 'django_mailman3.email_verify_adapter.NoNewUsersAccountAdapter'
+
+
 # LDAP configuration
 
 import ldap
-from django_auth_ldap.config import LDAPSearch, GroupOfNamesType
+from django_auth_ldap.config import LDAPSearch, GroupOfNamesType, LDAPSearchUnion, ActiveDirectoryGroupType
 
-INSTALLED_APPS = [
-            'django_auth_ldap',
-        ] + DEFAULT_APPS
 
 # Baseline configuration.
 AUTH_LDAP_SERVER_URI = "ldaps://authusm.usm.edu"
 AUTH_LDAP_BIND_DN = "CN=LINUXPROXY,OU=Service Accounts,OU=Domain Administration,DC=usm,DC=golden,DC=eagles"
 AUTH_LDAP_BIND_PASSWORD = "fri10&mar06?"
 
+
+AUTH_LDAP_FIND_GROUP_PERMS = True
+
 # Let's start with this, and we'll add the multiple search base config shown below later on.
-AUTH_LDAP_USER_SEARCH = LDAPSearch(
-    "OU=Domain Users,DC=usm,DC=golden,DC=eagles", ldap.SCOPE_SUBTREE, "(samAccountName=%(user)s)"
+#AUTH_LDAP_USER_SEARCH = LDAPSearch(
+#    "OU=Domain Users,DC=usm,DC=golden,DC=eagles", ldap.SCOPE_SUBTREE, "(userPrincipalName=%(user)s)"
+#)
+
+#AUTH_LDAP_USER_SEARCH = LDAPSearch(
+#    "OU=Administrative Accounts,OU=Domain Administration,DC=usm,DC=golden,DC=eagles", ldap.SCOPE_SUBTREE, "(userPrincipalName=%(user)s)",
+#)
+
+
+#Multiple Search Bases Celso 08/07/23
+AUTH_LDAP_USER_SEARCH = LDAPSearchUnion(
+    LDAPSearch("OU=Administrative Accounts,OU=Domain Administration,DC=usm,DC=golden,DC=eagles", ldap.SCOPE_SUBTREE, "(userPrincipalName=%(user)s)"),
+    LDAPSearch("OU=Domain Users,DC=usm,DC=golden,DC=eagles", ldap.SCOPE_SUBTREE, "(userPrincipalName=%(user)s)"),
 )
 
-# Multiple Search Bases
-#AUTH_LDAP_USER_SEARCH = LDAPSearchUnion(
-#    "OU=Administrative Accounts,OU=Domain Administration,DC=usm,DC=golden,DC=eagles", ldap.SCOPE_SUBTREE, "(samAccountName=%(user)s)"
-#    "OU=Domain Users,DC=usm,DC=golden,DC=eagles", ldap.SCOPE_SUBTREE, "(samAccountName=%(user)s)"
-#)
 # Or:
 # AUTH_LDAP_USER_DN_TEMPLATE = 'uid=%(user)s,ou=users,dc=example,dc=com'
 
@@ -62,7 +75,7 @@ AUTH_LDAP_GROUP_SEARCH = LDAPSearch(
     ldap.SCOPE_SUBTREE,
     "(objectClass=group)",
 )
-AUTH_LDAP_GROUP_TYPE = GroupOfNamesType(name_attr="cn")
+AUTH_LDAP_GROUP_TYPE = ActiveDirectoryGroupType(name_attr="cn")
 
 
 # We could create a mailman user group to restrict who can access it. I guess this
@@ -74,8 +87,11 @@ AUTH_LDAP_GROUP_TYPE = GroupOfNamesType(name_attr="cn")
 #AUTH_LDAP_DENY_GROUP = "cn=disabled,ou=django,ou=groups,dc=example,dc=com"
 
 # Populate the Django user from the LDAP directory.
+
+    # this is for just wID login
+    #"uid" : "samAccountName",
 AUTH_LDAP_USER_ATTR_MAP = {
-    "uid" : "samAccountName",
+    "uid" : "userPrincipalName",
     "first_name": "givenName",
     "last_name": "sn",
     "email" : "mail",
@@ -84,11 +100,12 @@ AUTH_LDAP_USER_ATTR_MAP = {
 
 # We can create Mailman Admin group and add it here
 
-#AUTH_LDAP_USER_FLAGS_BY_GROUP = {
-#    "is_active": "CN=App_Mailman_Users,OU=Application Groups,OU=Domain Groups,DC=usm,DC=golden,DC=eagles",
-#    "is_staff": "cn=App_Mailman_Staff,ou=django,ou=groups,dc=example,dc=com",
-#    "is_superuser": "CN=App_Mailman_Admins,OU=Application Groups,OU=Domain Groups,DC=usm,DC=golden,DC=eagles",
-#}
+AUTH_LDAP_USER_FLAGS_BY_GROUP = {
+    "is_staff": "CN=APP_MAILMAN_STAFF,OU=Application Groups,OU=Domain Groups,DC=usm,DC=golden,DC=eagles",
+    "is_superuser": "CN=APP_MAILMAN_SUPERUSERS,OU=Application Groups,OU=Domain Groups,DC=usm,DC=golden,DC=eagles",
+    #"is_active": "CN=App_Mailman_Users,OU=Application Groups,OU=Domain Groups,DC=usm,DC=golden,DC=eagles",
+}
+    #"is_active": "CN=App_Mailman_Users,OU=Application Groups,OU=Domain Groups,DC=usm,DC=golden,DC=eagles",
 
 # This is the default, but I like to be explicit.
 AUTH_LDAP_ALWAYS_UPDATE_USER = True
@@ -102,7 +119,12 @@ AUTH_LDAP_CACHE_TIMEOUT = 3600
 
 # Keep ModelBackend around for per-user permissions and maybe a local
 # superuser.
+
 AUTHENTICATION_BACKENDS = [
     "django_auth_ldap.backend.LDAPBackend",
     "django.contrib.auth.backends.ModelBackend",
 ]
+
+
+
+# DEBUG_TEMPLATE = False
